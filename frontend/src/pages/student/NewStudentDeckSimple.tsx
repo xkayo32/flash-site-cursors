13be9758ea076e208b1380fbc509f88c2decde3c
Kayo <kayo.fernandes_dev@outlook.com>
2025-08-28 10:28:02 +0000

    fix: corrigir sintaxe dos operadores ternários nos botões de loading
    - Remover chave dupla nos fechamentos dos operadores ternários
    - Mudar de )} para ) em todos os fechamentos de fragmentos JSX
    - Corrigir erros de compilação do esbuild

--- frontend/src/pages/student/NewStudentDeckSimple.tsx
+++ frontend/src/pages/student/NewStudentDeckSimple.tsx
@@ -341,7 +341,7 @@ export default function NewStudentDeckSimple() {
                       </label>
                     ))}
                   </div>
-                )}
+                )
               </div>
               
               {selectedFlashcards.length > 0 && (
@@ -387,7 +387,7 @@ export default function NewStudentDeckSimple() {
                     <Save className="w-4 h-4 mr-2" />
                     CRIAR DECK
                   </>
-                )}}
+                )}
               </Button>
             </div>
           </CardContent>
@@ -476,7 +476,7 @@ export default function NewStudentDeckSimple() {
                       />
                     </div>
                   </>
-                )}}
+                )}
                 
                 {newFlashcard.type === 'basic_inverted' && (
                   <>
@@ -517,7 +517,7 @@ export default function NewStudentDeckSimple() {
                       />
                     </div>
                   </>
-                )}}
+                )}
                 
                 {newFlashcard.type === 'cloze' && (
                   <>
@@ -541,7 +541,7 @@ export default function NewStudentDeckSimple() {
                       </p>
                     </div>
                   </>
-                )}}
+                )}
                 
                 {newFlashcard.type === 'multiple_choice' && (
                   <>
@@ -585,7 +585,7 @@ export default function NewStudentDeckSimple() {
                       </div>
                     ))}
                   </>
-                )}}
+                )}
                 
                 {newFlashcard.type === 'true_false' && (
                   <>
@@ -628,7 +628,7 @@ export default function NewStudentDeckSimple() {
                       />
                     </div>
                   </>
-                )}}
+                )}
                 
                 {newFlashcard.type === 'type_answer' && (
                   <>
@@ -669,7 +669,7 @@ export default function NewStudentDeckSimple() {
                       />
                     </div>
                   </>
-                )}}
+                )}
                 
                 {newFlashcard.type === 'image_occlusion' && (
                   <>
@@ -696,9 +696,9 @@ export default function NewStudentDeckSimple() {
                           />
                         </div>
                       </div>
-                    )}
+                    )
                   </>
-                )}}
+                )}
                 
                 {/* Dificuldade */}
                 <div>
